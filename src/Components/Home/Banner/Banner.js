a1e90bc4c50dfb1822efa2f2f3e44aa1d6d04b97
TowsifHasan <towsiafehasan007@gmail.com>
2021-11-13 22:33:02 +0600

    final-touch

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -14,19 +14,3 @@ export default Banner;
 
 
  
-//   2.https://i.ibb.co/xsRBTZN/red-luxury-sedan-road.jpg
-// 3. https://i.ibb.co/c18xRrs/photo-1552519507-da3b142c6e3d.jpg
-
-
-//    [
-//      {id:1, name:"Super car delux", price:4500000, description:"This is the Super car. If you Ride this car, you will feel like a hero", img:"https://i.ibb.co/cNvMLCy/white-offroader-jeep-parking.jpg"},
-//      {id:2, name:"Super Marcidies", price:5500000, description:"This is the Marcides car. If you Ride this car, you will feel like a hero. You will be a rich man", img:"https://i.ibb.co/xsRBTZN/red-luxury-sedan-road.jpg"},
-//      {id:3, name:"Super sedan car", price:6500000, description:"This is the Super sedan car. It's a dream car for all people. Only Rich people enjoy it.", img:"https://i.ibb.co/c18xRrs/photo-1552519507-da3b142c6e3d.jpg"},
-//      {id:4, name:"Audi car", price:7500000, description:"This is the Audi car. If you ride this car, you will feel like a hero. it is a awsome car. It's a good looking car.", img:"https://i.ibb.co/x5qV4xL/blue-sport-sedan-parked-yard.jpg"},
-//      {id:5, name:"Roles Royes", price:8500000, description:"This is the Roles Royes. If you ride this car, you will enjoy it. It's a beautiful car.", img:"https://i.ibb.co/VNt57Ft/84522.jpg"},
-//      {id:6, name:"Toyota", price:15000000, description:"This is the Toyota. If you ride this car, you will feel comfort.It is a long lasting car", img:"https://i.ibb.co/C73JCs6/67418.jpg"},
-//      {id:7, name:"BMW", price:25000000, description:"This is the BMW.It is one of the best car in the world. It is a costly car", img:"https://i.ibb.co/8NMk1tF/67413.jpg"},
-//      {id:8, name:"Honda Car", price:5000000, description:"This is the Honda Car. It is made by japan. It is a good and cheap car", img:"https://i.ibb.co/RC0nYMw/Orange-Elegant-Convertible-Car.jpg"},
-//      {id:9, name:"Nano car", price:9500000, description:"This is the Nano car. It is a family car. You can ride with your family.", img:"https://i.ibb.co/pbjBvfD/Car-led-lights-realistic-composition-with-dark-silhouette-of-automobile-with-dimmed-headlights-and-s.jpg"},
-//      {id:10, name:"Ferrari Car", price:5700000, description:"This is the Ferrari Car. It is one of the fast car in the world. It is a racing car.", img:"https://i.ibb.co/NLvH9XQ/59-social-media-post-template.jpg"}
-//    ]
